4d0442ef875750299948886b23ab5bb744d8fd16
Susie Stanley <susiestanley00@gmail.com>
2022-01-17 11:53:26 -0600

    updated about me section

--- src/components/About.js
+++ src/components/About.js
@@ -12,12 +12,22 @@ export const About = () => {
           <div className="box2">
             <h1>About &nbsp; Me</h1>
             <p>
-              Chocolate cake sweet carrot cake macaroon dessert. Brownie muffin
-              chocolate bar gingerbread pudding gingerbread. Tart shortbread
-              jelly-o topping pie donut bear claw. Sweet topping croissant jelly
-              beans marshmallow gummi bears. Tart marshmallow toffee chocolate
-              cake cotton candy lemon drops cupcake. Chocolate cake sweet carrot
-              cake macaroon dessert.
+              In my time at Nashville Software School, I've been lucky to learn
+              not just programming languages, but more importantly, HOW to learn
+              and how to use all the resources available. My teammates describe
+              me as a problem-solver, good communicator, curious, creative,
+              organized, independent, driven, and that I have a highly tuned
+              attention to detail (down to the pixel). There is something deeply
+              rewarding about bringing my visions to life. I've found that
+              development is an art-form using my keyboard instead of paint
+              &amp; brushes.{" "}
+            </p>
+            <p>
+              In my job search, finding a good culture fit is at the top of my
+              list. When I join any endeavor, I’m ALL IN, so knowing that the
+              whole team is rowing in the same direction is vital. I would love
+              to continue working in a remote position as my home office is my
+              ideal environment for productivity and efficiency.
             </p>
           </div>
         </section>
